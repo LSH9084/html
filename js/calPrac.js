aad681f395f320dadc82f07e566274cde7293c28
LSH9084 <sanghun1101088@gmail.com>
2024-05-16 18:17:48 +0900

    .

---
+++ js/calPrac.js
@@ -0,0 +1,29 @@
+const output1 = document.querySelector('.output1')
+const cle = document.querySelector('.clear')
+const opera = document.querySelectorAll('.operator')
+const btns = document.querySelectorAll('.btns')
+const result = document.querySelector('.result')
+
+cle.addEventListener('click',(e)=>{
+    output1.value = ''
+})
+
+btns.forEach((v)=>{
+    v.addEventListener('click',(e)=>{
+        output1.value += e.target.value
+    })
+})
+
+opera.forEach((v)=>{
+    v.addEventListener('click',(e)=>{
+        output1.value += e.target.value
+    })
+})
+
+result.addEventListener('click',(e)=>{
+    output1.value = eval(output1.value)
+
+})
+
+
+
